757c67029417c31800eea6437240f6f840138d6c
avibharadwaj <a.bharadwaj@somaiya.edu>
2020-01-24 21:24:10 +0530

    added sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+  
+import React, { Component } from 'react';
+import { CreateAccountScreen } from './CreateAccountScreen';
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CreateAccountScreen />
+      </div>
+    );
+  }
 }
 
 export default App;
