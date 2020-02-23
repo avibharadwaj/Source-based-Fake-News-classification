5de7c5fd087cbe12d5b6aa6c9e8276a3f153c9e0
avibharadwaj <a.bharadwaj@somaiya.edu>
2020-02-23 12:36:10 +0530

    front end integration left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-  
-import React, { Component } from 'react';
-import { CreateAccountScreen } from './CreateAccountScreen';
+import React from 'react';
+import {BrowserRouter,Route} from "react-router-dom";
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import Main from './Main';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CreateAccountScreen />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className = "App">
+    	<BrowserRouter>
+        <div>
+        <Route path="/" exact component={SignUp} />
+        <Route path="/SignIn" exact component={SignIn} />
+        <Route path="/Main" exact component={Main} />
+        </div> 
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
