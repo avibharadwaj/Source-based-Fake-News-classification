e7113be5c345aa9e94073eadd8fc82f4db8d7be5
parshva-b <parshva.barbhaya@somaiya.edu>
2020-02-24 12:40:28 +0530

    Requirements freezed, Readme.md updated

--- README.md
+++ README.md
@@ -17,3 +17,18 @@
 
 Check google Drive for datacleaning process
 - [@feature_selection](https://docs.google.com/document/d/126HW_b7yylWT2_eS_NYvPheeg9P0sMVUjAMaamhPEZ4/edit?usp=sharing)
+
+
+## Requirements:
+
+1.  Python:
+	a. Python3 
+	b. Run command ```pip3 freeze -r requirements.txt``` (python folder)
+
+2. Backend:
+	a. Install node, yarn
+	b. yarn add (in backend folder)
+
+*Note*: To run both python and backend servers need to be running
+1. Python: (in python sub dir) -> python3 app.py
+2. Backend: (in backend folder) -> npm run watch / npm start
--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
-const newsRouter = require('./controllers/routes')
+const routes = require('./controllers/routes')
 const middleware = require('./utils/middleware')
 
 app.use(cors())
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 morgan.token('body', function(res, req) { return JSON.stringify( req.body ) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use('/', newsRouter)
+app.use('/', routes)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
