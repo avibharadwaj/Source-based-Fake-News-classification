51aa351b765c1a3fbdfb6257ce0ada6b1314af3c
parshva-b <parshva.barbhaya@somaiya.edu>
2020-06-23 00:01:26 +0530

    Restructed and all route added

--- python/app.py
+++ python/app.py
@@ -1,26 +1,24 @@
 from flask import Flask, jsonify, request, abort
 from newspaper import Article
 from langdetect import detect
-from urllib.parse import urlparse
-from iso639 import languages
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from model import predictResult
+from web_scraper import scrape
 
-import pickle
-import re
+import pickle, re, json
 
 # tf_idf = pickle.load(open("history_tfidf.pkl", "rb"))
 # svm = pickle.load(open("histroy_svm.pkl", "rb"))
 # naive_bayes = pickle.load(open("history_naive_bayes.pkl","rb"))
 
-tf_idf = pickle.load(open("tfidf.pkl", "rb"))
-svm = pickle.load(open("SVM.pkl", "rb"))
-naive_bayes = pickle.load(open("naive_bayes.pkl","rb"))
-random_forest = pickle.load(open("random_forest.pkl", "rb"))
-adaboost = pickle.load(open("adaboost.pkl", "rb"))
-logistic_regression = pickle.load(open("logistic_regression.pkl", "rb"))
-basic_neural_network = pickle.load(open("basic_neural_network.pkl", "rb"))
+tf_idf = pickle.load(open("models/tfidf.pkl", "rb"))
+svm = pickle.load(open("models/SVM.pkl", "rb"))
+naive_bayes = pickle.load(open("models/naive_bayes.pkl","rb"))
+random_forest = pickle.load(open("models/random_forest.pkl", "rb"))
+adaboost = pickle.load(open("models/adaboost.pkl", "rb"))
+logistic_regression = pickle.load(open("models/logistic_regression.pkl", "rb"))
+basic_neural_network = pickle.load(open("models/basic_neural_network.pkl", "rb"))
 
 app = Flask(__name__)
 
@@ -29,69 +27,8 @@ def index():
 	#error handling yet to be done
 	print("Inside extraction")
 	print(request)
-	if not request.json or not 'url' in request.json:
-		abort(400)
-	url = request.json['url']
-	article = Article(url)
+	return scrape(request)
 	
-	article.download()
-	article.parse()
-	article.nlp()
-	
-	print("Publish date:")
-	print(article.publish_date)
-
-	#authors of article
-	authors = []
-	print("Authors:")
-	for x in article.authors:
-		authors.append( x )
-	
-	content = []
-	lines = article.text.split('\n')
-	print("Article text:")
-	for x in lines:
-		if x != "AD":
-			content.append(x)
-	
-	#text of article
-	text = ''.join(content)
-	# print(text)
-
-	#title of article
-	print("Article Title:")
-	title = article.title
-
-	#hasImage in article
-	print("Has Image:")
-	hasImage = 0
-	if article.images:
-		hasImage = 1
-
-	#language of article
-	print("Language of article:")
-	language = detect(text)
-
-	#mainsite URL
-	#find a better way than [4:]
-	print("Main Site URL:")
-	mainSiteURL = (urlparse(url).netloc)[4:]
-	print(mainSiteURL)
-
-	##extract keywords
-	print("Keyword extraction:")
-	print(article.keywords)
-
-	return jsonify(
-		authors = article.authors,
-		text = text,
-		title = title,
-		hasImage = hasImage,
-		language = (languages.get(alpha2 = language)).name,
-		site_url = mainSiteURL,
-		published = article.publish_date,
-		keywords = article.keywords
-	)
 
 @app.route('/api/naive', methods = ['POST'])
 def predNaive():
@@ -140,6 +77,24 @@ def predNN():
 		abort(400)
 	text = re.sub(r'\d+','', request.json['text'])
 	return predictResult(text, tf_idf, basic_neural_network)
+
+@app.route('/api/all', methods = ['POST'])
+def all():
+	print(request)
+	body = scrape(request)
+	
+	if not body.json or not 'text' in body.json:
+		abort(400)
+	text = re.sub(r'\d+','', body.json['text'])
+	ml = {}
+	ml['nn'] = predictResult(text, tf_idf, basic_neural_network).json['result']
+	ml['logistic'] = predictResult(text, tf_idf, logistic_regression).json['result']
+	ml['rf'] = predictResult(text, tf_idf, random_forest).json['result']
+	ml['ada'] = predictResult(text, tf_idf, adaboost).json['result']
+	ml['svm'] = predictResult(text, tf_idf, svm).json['result']
+	ml['naive'] = predictResult(text, tf_idf, naive_bayes).json['result']
+	print(ml)
+	return jsonify(ml)
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- python/model.py
+++ python/model.py
@@ -3,8 +3,8 @@ import pickle
 
 def predictResult(text, tfidf, model):
 	text_features = tfidf.transform([text])
-	print(text_features.shape)
+	# print(text_features.shape)
 	# print(text_features)
 	result = model.predict(text_features)
-	# print(naive_result)
-	return jsonify(result[0])
+	# print(result)
+	return jsonify(result = result[0])
